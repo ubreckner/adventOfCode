bc2a6f61d98bac002ad34d7e2d7549a943bd6dfe
v1per <v1per@bla.de>
2021-12-04 02:37:52 +0100

    Day 3.2

--- src/main/java/de/v1per/adventofcode/days/Day3.java
+++ src/main/java/de/v1per/adventofcode/days/Day3.java
@@ -25,25 +25,59 @@ public class Day3 extends Day<String> {
         for (int i = 0; i < firstRow.length(); i++) {
             int finalI = i;
             Optional<Map.Entry<String, Long>> mostCommonBit = this.list.stream()
-                                                                       .collect(Collectors.groupingBy(
-                                                                               s -> s.substring(finalI, finalI + 1),
-                                                                               Collectors.counting()))
-                                                                       .entrySet()
-                                                                       .stream()
-                                                                       .max(Map.Entry.comparingByValue());
+                    .collect(Collectors.groupingBy(s -> s.substring(finalI, finalI + 1), Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    .max(Map.Entry.comparingByValue());
             gammaRateStr.append(mostCommonBit.get().getKey());
         }
+        //Least common bits are the 2-complement of the gamma bits
         String epsilonRateStr = Arrays.stream(gammaRateStr.toString().split(""))
-                                      .map(s -> s.equals("0") ? "1" : "0")
-                                      .collect(Collectors.joining());
+                .map(s -> s.equals("0") ? "1" : "0")
+                .collect(Collectors.joining());
         gammaRate = Long.parseLong(gammaRateStr.toString(), 2);
-        epsilonRate = Long.parseLong(epsilonRateStr.toString(), 2);
+        epsilonRate = Long.parseLong(epsilonRateStr, 2);
 
         System.out.println("3.1: " + gammaRate * epsilonRate);
     }
 
     @Override
     void two() {
+        long o2Rating;
+        long co2Rating;
+        StringBuilder o2Str = new StringBuilder();
+        StringBuilder co2Str = new StringBuilder();
+        List<String> o2List = this.list;
+        List<String> co2List = this.list;
+        //boldly assume every row is the same size as the first
+        for (int i = 0; i < this.list.get(0).length(); i++) {
+            int finalI = i;
+            Optional<Map.Entry<String, Long>> mostCommonBit = o2List.stream()
+                    .collect(Collectors.groupingBy(s -> s.substring(finalI, finalI + 1), Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    //Special comparator to take into account what happens when there are equally many numbers left
+                    .max((Map.Entry<String, Long> e1, Map.Entry<String, Long> e2) -> ((e1.getValue() > e2.getValue()) || (e1.getValue()
+                            .equals(e2.getValue()) && e1.getKey().equals("1"))) ? 1 : -1);
 
+            o2Str.append(mostCommonBit.get().getKey());
+            o2List = o2List.stream().filter(s -> s.startsWith(o2Str.toString())).collect(Collectors.toList());
+
+            Optional<Map.Entry<String, Long>> leastCommonBit = co2List.stream()
+                    .collect(Collectors.groupingBy(s -> s.substring(finalI, finalI + 1), Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    //Special comparator to take into account what happens when there are equally many numbers left
+                    .min((Map.Entry<String, Long> e1, Map.Entry<String, Long> e2) -> ((e1.getValue() > e2.getValue()) || (e1.getValue()
+                            .equals(e2.getValue()) && e1.getKey().equals("1"))) ? 1 : -1);
+
+            co2Str.append(leastCommonBit.get().getKey());
+            co2List = co2List.stream().filter(s -> s.startsWith(co2Str.toString())).collect(Collectors.toList());
+        }
+
+        o2Rating = Long.parseLong(o2Str.toString(), 2);
+        co2Rating = Long.parseLong(co2Str.toString(), 2);
+        System.out.println("3.2: " + o2Rating * co2Rating);
     }
+
 }
